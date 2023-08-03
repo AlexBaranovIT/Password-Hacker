54181623d6fd3421e73aba884581e6e7be6e8b47
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-08-03 15:56:13 +0400

    Update passwordhack.py

--- passwordhack.py
+++ passwordhack.py
@@ -1,5 +1,6 @@
 from string import ascii_letters, digits, punctuation
 from itertools import product
 
+#Cycle for genertaing all possible passwords
 for passcode in product(ascii_letters + digits + punctuation, repeat=4):
   print(*passcode)
